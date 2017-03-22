41a57144169a5c8f69f0edae32c23d4851861666
Dominick Houston <dominick.houston14@gmail.com>
2017-03-22 12:33:52 -0400

    4/5 CRD no U

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set("port", process.env.PORT || 3001);
 app.set("view engine", "hbs");
 
 app.use("/assets", express.static("public"));
-app.use(parser.urlencoded({extended: true}));
+app.use(parser.json({extended: true}));
 
 app.engine(".hbs", hbs({
   extname:        ".hbs",
@@ -20,29 +20,50 @@ app.engine(".hbs", hbs({
   defaultLayout:  "layout-main"
 }));
 
+// Root
 app.get("/", (req, res) => {
   res.render("cars");
   // res.json(__dirname + "/index.html");
 })
 
+// Find all "Cars?"
 app.get("/api/cars", function(req, res) {
   Car.find({}).then(function(cars) {
     res.json(cars)
   });
 });
 
+// Find one and Show details
 app.get("/api/cars/:car", function(req, res) {
   Car.findOne({car: req.params.car}).then(function(car){
     res.json(car);
   });
 });
 
+// Create
 app.post("/api/cars", function(req, res){
-  Car.create(req.body.car).then(function(car){
+  Car.create(req.body).then(function(car){
     res.json(car);
   });
 });
 
+// change app.post to app.delete for DELETE
+app.delete("/api/cars/:car", function(req, res){
+  Car.findOneAndRemove({car: req.params.car}).then(function(){
+    res.json({success: true});
+    // res.redirect("/candidates");
+  });
+});
+
+// change app.post to app.put for UPDATE
+app.put("/api/cars/:car", function(req, res){
+  Car.findOneAndUpdate({car: req.params.car}, req.body.car, {new: true}).then(function(car){
+    res.json({success: true});
+    // res.redirect("/candidates/" + candidate.name);
+  });
+});
+
+// Listen and Port 3001 (See above Line ...)
 app.listen(app.get("port"), () => {
   console.log("The Mean Dom App is Connected");
 });
--- public/js/app.js
+++ public/js/app.js
@@ -18,24 +18,49 @@ angular
   .controller("CarShowController", [
     "CarFactory",
     "$stateParams",
+    "$state",
     CarShowControllerFunction
   ])
+  .controller("CarNewController", [
+    "CarFactory",
+    "$state",
+    CarNewControllerFunction
+  ])
+  // .controller("CarEditController", [
+  //   "CarFactory",
+  //   "$state",
+  //   "$stateParams",
+  //   CarEditControllerFunction
+  // ])
 
   function Router($stateProvider) {
     $stateProvider
     .state("index", {
-        url: "/cars",
-        templateUrl: "/assets/js/ng-views/index.html",
-        controller: "CarIndexController",
-        controllerAs: "vm"
+      url: "/cars",
+      templateUrl: "/assets/js/ng-views/index.html",
+      controller: "CarIndexController",
+      controllerAs: "vm"
       })
     .state("show", {
-        url: "/cars/:car",
-        templateUrl: "/assets/js/ng-views/show.html",
-        controller: "CarShowController",
-        controllerAs: "vm"
+      url: "/cars/:car",
+      templateUrl: "/assets/js/ng-views/show.html",
+      controller: "CarShowController",
+      controllerAs: "vm"
       })
-    }
+    .state("new", {
+      url: "/cars",
+      templateUrl: "/assets/js/ng-views/new.html",
+      controller: "CarNewController",
+      controllerAs: "vm"
+    })
+    .state("edit", {
+      url: "/cars/:car/edit",
+      templateUrl: "/assets/js/ng-views/edit.html",
+      controller: "CarEditController",
+      controllerAs: "vm"
+    })
+
+  }
 
   function CarFactoryFunction($resource) {
   return $resource("/api/cars/:car", {}, {
@@ -51,6 +76,33 @@ function CarIndexControllerFunction(CarFactory) {
   this.cars = CarFactory.query()
 }
 
-function CarShowControllerFunction(CarFactory, $stateParams) {
+function CarShowControllerFunction(CarFactory, $stateParams, $state) {
   this.car = CarFactory.get({car: $stateParams.car})
+  this.update = function() {
+    this.car.$update({car: $stateParams.car}).then(function() {
+      $state.go("index")
+    })
+  }
+  this.destroy = function() {
+    this.car.$delete({car: $stateParams.car}).then(function() {
+      $state.go("index");
+    })
+  }
 }
+
+function CarNewControllerFunction(CarFactory, $state) {
+  this.car = new CarFactory();
+  this.create = function() {
+    this.car.$save(function(car) {
+      $state.go("index", {car: car.car})
+    })
+  }
+}
+// function CarEditControllerFunction(CarFactory, $state, $stateParams) {
+//   this.car = new CarFactory();
+//   this.create = function() {
+//     this.car.$save(function(car) {
+//       $state.go("index", {car: car.car})
+//     })
+//   }
+// }
